3eedeea19f06964091503b2cb955b776339e2445
pokbab <xxxtintin@naver.com>
2014-09-23 00:07:41 +0900

    mybatis 연동작업 HSQL 연동. (임시사용)

--- src/main/java/com/baron/sample/dao/SampleDAOImpl.java
+++ src/main/java/com/baron/sample/dao/SampleDAOImpl.java
@@ -1,15 +1,14 @@
 package com.baron.sample.dao;
 
-import javax.annotation.Resource;
-
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SampleDAOImpl implements SampleDAO {
-	private static final String NAMESPACE = "com.nhnent.admin.AdminModel.";
+	private static final String NAMESPACE = "com.baron.sample.SampleModel.";
 
-	@Resource(name = "sqlSession")
+	@Autowired
 	private SqlSession session;
 	
 	@Override
--- src/test/java/com/baron/sample/dao/SampleDAOImplTest.java
+++ src/test/java/com/baron/sample/dao/SampleDAOImplTest.java
@@ -4,25 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/applicationContext.xml")
+@ContextConfiguration("/applicationContext-test.xml")
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 @Transactional
 public class SampleDAOImplTest {
+	
+	@Autowired
+	private SampleDAO dao;
 
 	@Test
 	public void test() throws Exception {
 		//Given
-		
 
 		//When
+		int actual = dao.selectTest();
 
 		//Then
-
+		assertEquals(2, actual);
 	}
 }
